83df32b48edfcd89b555a0733c9e063009001ed2
Alexander Ponomarenko <contact@propulsion-analysis.com>
2015-03-14 19:22:46 +0100

    Updated build system

--- example/pages/build.sh
+++ example/pages/build.sh
@@ -4,7 +4,3 @@ fastcgi3-page-compiler -updateFactory=true -updateMakefile=true *.cpsp
 ./autogen.sh
 ./configure
 make	
-cp ./.libs/server_pages.so.0.0.0 ../
-ln -sf ../server_pages.so.0.0 ../server_pages.so
-ln -sf ../server_pages.so ../server_pages.so.0
-
